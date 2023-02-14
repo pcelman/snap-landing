eb9a8cae292960f399c1d648dec4b25f23c852c9
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-02-14 14:09:37 -0300

    menu

---
+++ js/menu.js
@@ -0,0 +1,56 @@
+const menuIcon = document.querySelector(".nav__hamb");
+const navOverlay = document.querySelector("nav__overlay");
+let currentDropdown = navOverlay;
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("nav__hamb--open");
+  navOverlay.classList.toggle("nav__overlay--show");
+});
+
+
+navOverlay.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentElement = e.target;
+  // console.log(e.target.classList.value);
+
+  if (isActive(currentElement, "nav__parent")) {
+    const subMenu = currentElement.parentElement.children[1];
+    console.log(subMenu);
+
+    if (window.innerWidth < 768) {
+      let height = subMenu.clientHeight == 0 ? subMenu.scrollHeight : 0;
+      // console.log(subMenu.clientHeight);
+
+      subMenu.style.height = `${height}px`;
+    } else {
+      if (!isActive(subMenu, "nav__inner--show")) {
+        closeDropdown(currentDropdown);
+      }
+      subMenu.classList.toggle("nav__inner--show");
+
+      currentDropdown = subMenu;
+    }
+  }
+});
+
+function isActive(element, string) {
+  return element.classList.value.includes(string);
+}
+
+function closeDropdown(currentDropdown) {
+  if (isActive(currentDropdown, "nav__inner--show")) {
+    currentDropdown.classList.remove("nav__inner--show");
+  }
+}
+
+window.addEventListener("resize", () => {
+  closeDropdown(currentDropdown);
+
+  if (window.innerWidth > 768) {
+    const navInners = document.querySelectorAll(".nav__inner");
+
+    navInners.forEach((navInner) => {
+      navInner.style.height = "";
+    });
+  }
+});
